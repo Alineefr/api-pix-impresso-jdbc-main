ef0f2a557e6930ec74d41080fbf6682c3bd5a992
Aline Frescura <alinefrescura69@gmail.com>
2024-06-11 21:55:59 -0300

    commit

--- src/main/java/br/senac/pr/api_pix_impresso/controllers/CaixaController.java
+++ src/main/java/br/senac/pr/api_pix_impresso/controllers/CaixaController.java
@@ -3,12 +3,15 @@ package br.senac.pr.api_pix_impresso.controllers;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.senac.pr.api_pix_impresso.dtos.CreateCaixaDto;
+import br.senac.pr.api_pix_impresso.dtos.UpdateCaixaDto;
 import br.senac.pr.api_pix_impresso.models.Caixa;
 import br.senac.pr.api_pix_impresso.services.CaixaService;
 
@@ -39,7 +42,18 @@ public class CaixaController {
   }
 
   // PATCH
-  // PUT
+  @PatchMapping("/{id}")
+  public Caixa updateCaixa(@RequestBody UpdateCaixaDto dto,
+      @RequestParam long id) {
+    // Cria um objeto do modelo
+    Caixa caixa = new Caixa(id,
+        dto.getLocalizacao(),
+        dto.getSaldo());
+
+    caixaService.update(caixa);
+    return caixa;
+  }
+  // PUT - Atualização completa
   // DELETE
 
 }
--- src/main/java/br/senac/pr/api_pix_impresso/services/CaixaService.java
+++ src/main/java/br/senac/pr/api_pix_impresso/services/CaixaService.java
@@ -11,18 +11,36 @@ import br.senac.pr.api_pix_impresso.repositories.JdbcCaixaRepository;
 public class CaixaService {
   private JdbcCaixaRepository caixaRepository;
 
-public CaixaService(JdbcCaixaRepository caixaRepository) {
+  public CaixaService(JdbcCaixaRepository caixaRepository) {
     this.caixaRepository = caixaRepository;
-} 
-  
-  public int save (Caixa caixa) {
-    if  (caixa == null) {
-        throw new Error("Dados do caixa inválidos");
+  }
+
+  public int save(Caixa caixa) {
+    // regras de negócio
+    if (caixa == null) {
+      throw new Error("Dados do caixa inválidos");
+    }
+
+    if (caixa.getSaldo() <= 0) {
+      throw new Error("Saldo não pode ser menor ou igual a zero");
     }
     return caixaRepository.save(caixa);
   }
 
-  public List<Caixa> findAll(){
+  public List<Caixa> findAll() {
     return caixaRepository.findAll();
   }
+
+  public int update(Caixa caixa) {
+    if (caixa == null) {
+      throw new Error("Dados do caixa inválidos");
+    }
+
+    if (caixa.getId() <= 0 || caixa.getId() == null) {
+      throw new Error("ID do caixa inválido");
+    }
+    // TODO - Finalizar o método update no repository
+    return caixaRepository.update(caixa);
+  }
+
 }
